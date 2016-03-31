8da2383ac0b80c57a9eefd96800331418be5c590
The Infinity Tree <subhabrata@theinfinitytree.com>
2016-03-31 18:27:03 +0530

    Added files via upload

---
+++ Room.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: subhabrata
+ * Date: 1/9/2016
+ * Time: 12:15 PM
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Room extends  CI_Controller {
+
+
+
+
+
+    }
